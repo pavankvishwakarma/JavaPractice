63fe908ea21d5d7d8d6a85303b6f5262561317cf
kumap16 <pavan.kumar@in.pega.com>
2018-10-11 23:09:17 +0530

    Adding my program

---
+++ src/Program1.java
@@ -0,0 +1,35 @@
+
+/*How do you find the duplicate number on a given integer array?*/
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Program1 {
+	
+	public static int duplicateInteger(int a[]){
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for (int i = 0; i < a.length; i++) {
+			if(hm.containsKey(a[i])){
+				return a[i];
+			}else{
+				hm.put(a[i], 1);
+			}
+			
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the size of the integer");
+		int size = s.nextInt();
+		System.out.println("Enter the integers of the array");
+		int a[]=new int[size];
+		for (int i = 0; i < size; i++) {
+			int n= s.nextInt();
+			a[i]= n;
+		}
+		System.out.println(-1==duplicateInteger(a)?"No duplicate":duplicateInteger(a));
+		
+	}
+}
